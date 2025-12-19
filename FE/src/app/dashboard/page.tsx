6f8f7f7ac27557614b74119630c47d62bc9c7df7
Prasad <prasadjadhav510@gmail.com>
2025-12-20 01:55:02 +0530

    file rename

--- FE/src/app/dashboard/page.tsx
+++ FE/src/app/dashboard/page.tsx
@@ -1,4 +1,4 @@
-import { AppSidebar } from "@/components/app-sidebar"
+import { AppSidebar } from "../../components/app-sidebar"
 import { SiteHeader } from "@/components/site-header"
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar"
 import { useHistory } from "@/hooks/use-history"
@@ -94,7 +94,7 @@ export default function Page() {
     }
 
     if (historyLoading) {
-        return <PageSkeleton />
+         return <PageSkeleton />
     }
 
     return (
